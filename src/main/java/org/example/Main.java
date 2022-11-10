589d825206d1e33bd258bbb888d0f2c7afbc34db
Vladimir Sidorenko <vladmir.sidorenko@gmail.com>
2022-11-10 20:11:28 +0300

    Release serverSocket

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    protected static int port = 8080;
+
+    public static void main(String[] args) throws IOException {
+            server(port);
+    }
+
+    public static void server(int port) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Запуск сервера");
+            try (Socket clientSocket = serverSocket.accept()) {
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("New connect!");
+                String name = in.readLine();
+                System.out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+            }
+        }
+    }
+}
