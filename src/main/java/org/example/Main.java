e831b81b8093c76ad9b97e5c1da249579f4af101
Vladimir Sidorenko <vladmir.sidorenko@gmail.com>
2022-11-14 20:30:45 +0300

    Сorrection of a remark

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,15 +8,15 @@ public class Main {
     protected static int port = 8080;
 
     public static void main(String[] args) throws IOException {
-            server(port);
+        server(port);
     }
 
     public static void server(int port) throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Запуск сервера");
-            try (Socket clientSocket = serverSocket.accept()) {
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            try (Socket clientSocket = serverSocket.accept();
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
                 System.out.println(in.readLine());
                 out.println("Добро пожаловать на сервер. Ваш порт: " + clientSocket.getPort());
 
